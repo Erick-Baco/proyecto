7dde19168cb226dbc8f8ea6fe8208a3260ae969e
Erick-Baco <jesusbaco542@gmail.com>
2022-11-26 17:27:36 -0600

    añadir plantillas base

---
+++ corrector/stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+// stdafx.h: archivo de inclusión de los archivos de inclusión estándar del sistema
+// o archivos de inclusión específicos de un proyecto utilizados frecuentemente,
+// pero rara vez modificados
+//
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: mencionar aquí los encabezados adicionales que el programa necesita#pragma once
