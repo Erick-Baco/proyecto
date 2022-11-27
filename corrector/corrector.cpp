252c3ee86ecdbdea85475d9400b790bb1abe93d2
Erick-Baco <jesusbaco542@gmail.com>
2022-11-26 21:42:37 -0600

    primera version de la funcion clonapalabras

--- corrector/corrector.cpp
+++ corrector/corrector.cpp
@@ -13,6 +13,9 @@
 #include "stdafx.h"
 #include <string.h>
 #include "corrector.h"
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
 //Funciones publicas del proyecto
 /*****************************************************************************************************************
 	DICCIONARIO: Esta funcion crea el diccionario completo
@@ -71,7 +74,115 @@ void	ClonaPalabras(
 	char	szPalabrasSugeridas[][TAMTOKEN], 	//Lista de palabras clonadas
 	int& iNumSugeridas)						//Numero de elementos en la lista
 {
-	//Sustituya estas lineas por su código
+	char palabra[50];
+	char palabraTemp[100];
+	int conta;
+	int limite = 0;
+	char alfabeto[] = "abcdefghijklmnopqrstuvwxyz";
+	int conta2, conta3, conta4;
+	char palabraEsp[100];
+
+
+
+	//FILE* fp;
+	//fopen_s(&fp, "candidatos.txt", "w");
+
+	scanf_s("%s", palabra, 49);
+
+	limite = 0;
+	while (palabra[limite] != '\0')
+	{
+		strcpy_s(palabraTemp, palabra);
+
+		conta = limite;
+		while (palabraTemp[conta] != '\0')
+		{
+			palabraTemp[conta] = palabraTemp[conta + 1];
+			conta++;
+		}
+		printf("%s", palabraTemp);
+		printf("\n");
+
+		limite++;
+	}
+
+	char temp;
+	limite = 0;
+	while (palabra[limite] != '\0')
+	{
+		strcpy_s(palabraTemp, palabra);
+
+		temp = palabraTemp[limite];
+		palabraTemp[limite] = palabraTemp[limite + 1];
+		palabraTemp[limite + 1] = temp;
+
+		printf("%s", palabraTemp);
+		printf("\n");
+
+		limite++;
+	}
+
+	conta2 = 0;
+	while (palabra[conta2] != '\0')
+	{
+		strcpy_s(palabraTemp, palabra);
+
+		limite = strlen(alfabeto);
+		conta = 0;
+		while (conta < limite)
+		{
+			palabraTemp[conta2] = alfabeto[conta];
+			printf("%s", palabraTemp);
+			printf("\n");
+			conta++;
+		}
+		conta2++;
+	}
+
+	char c;
+
+
+	conta = 0;
+	conta2 = 0;
+
+	limite = strlen(palabra);
+	while (conta <= limite)
+	{
+		c = palabra[conta];
+		palabraEsp[conta2] = ' ';
+		conta2++;
+		palabraEsp[conta2] = c;
+		conta++;
+		conta2++;
+	}
+
+
+	conta = 0;
+	conta2 = 0;
+	while (palabraEsp[conta] != '\0')
+	{
+		while (conta2 < 26)
+		{
+			palabraEsp[conta] = alfabeto[conta2];
+			conta3 = 0;
+			conta4 = 0;
+			while (palabraTemp[conta4] != '\0')
+			{
+				if (palabraEsp[conta3] != ' ')
+				{
+					palabraTemp[conta4] = palabraEsp[conta3];
+					conta4++;
+				}
+				conta3++;
+			}
+			conta2++;
+			printf("%s", palabraTemp);
+			printf("\n");
+
+		}
+		conta = conta + 2;
+	}
+
 	strcpy(szPalabrasSugeridas[0], szPalabraLeida); //lo que sea que se capture, es sugerencia
 	iNumSugeridas = 1;							//Una sola palabra sugerida
 }
